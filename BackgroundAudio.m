1135181e7a1ea488785ddd3ff7b68bf9f61dfe8d
aliendb <alienslanding@hotmail.com>
2017-03-06 01:24:55 -0500

    Add files via upload

---
+++ BackgroundAudio.m
@@ -0,0 +1,23 @@
+#import <Cordova/CDV.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface BackgroundAudio : CDVPlugin
+{}
+@end
+
+@implementation BackgroundAudio
+
+- (void)pluginInitialize {
+    // initializations go here.
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    BOOL ok;
+    NSError *setCategoryError = nil;
+    ok = [audioSession setCategory:AVAudioSessionCategoryPlayback
+                       withOptions:AVAudioSessionCategoryOptionMixWithOthers
+                             error:&setCategoryError];
+    if (!ok) {
+        NSLog(@"%s setCategoryError=%@", __PRETTY_FUNCTION__, setCategoryError);
+    }
+}
+
+@end
